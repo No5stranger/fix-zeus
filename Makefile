2de73f4bf14c1e718b791490252171c9ae8bbe2b
cjp <No5stranger@gmail.com>
2014-10-20 13:44:17 +0800

    project init

---
+++ Makefile
@@ -0,0 +1,3 @@
+test:
+	@echo ">-------- phpunit testing --------<"
+	vendor/bin/phpunit -c phpunit.xml.dist
---
+++ src/Factory.php
@@ -0,0 +1,15 @@
+<?php
+namespace Fixzeus;
+
+class Factory
+{
+    public function __construct()
+    {
+        d('cjp');
+    }
+
+    public function dump()
+    {
+        d('ok');
+    }
+}
