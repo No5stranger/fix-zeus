bb6a9494737cbe9e893c95351fb1770c424163aa
cjp <No5stranger@gmail.com>
2014-10-21 19:44:09 +0800

    add Service.php

---
+++ src/model/Service.php
@@ -0,0 +1,16 @@
+<?php
+namespace Fixzeus\Model;
+
+class Service
+{
+    public static $nSpace = array(
+        "ees" => "ElemeEmailService_",
+        "eos" => "ElemeOrderService_",
+        "ers" => "ElemeRestaurantService_",
+        "ess" => "ElemeSearchService_",
+        "eus" => "ElemeUserService_",
+        "geos" => "GeohashService_",
+        "oas" => "OpenAPIService_",
+        "sms" => "ShortMessageService_",
+    );
+}
